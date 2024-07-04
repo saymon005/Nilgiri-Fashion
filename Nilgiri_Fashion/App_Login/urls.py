dadb3989af4ea4aca4636ad0d986a05aaea23a58
saymon005 <msaymonislamiftikar@gmail.com>
2024-07-05 02:06:14 +0600

    Chane user Profile

--- Nilgiri_Fashion/App_Login/urls.py
+++ Nilgiri_Fashion/App_Login/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('signup/', views.sign_up, name='signup'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('profile/', views.user_profile, name='profile'),
 ]
--- Nilgiri_Fashion/App_Login/views.py
+++ Nilgiri_Fashion/App_Login/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Authentication
 from django.contrib.auth.forms import AuthenticationForm
@@ -40,3 +40,15 @@ def logout_user(request):
     logout(request)
     return HttpResponse('Logged Out')
 
+@login_required
+def user_profile(request):
+    profile = Profile.objects.get(user=request.user)
+
+    form = ProfileForm(instance=profile)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            form = ProfileForm(instance=profile)
+
+    return render(request, 'App_Login/change_profile.html', context={'form': form})        
